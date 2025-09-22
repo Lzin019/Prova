b85347eb89246372d6d42b5b663e087fa6c2a3d6
Luiz <luizfelipeallagedd@gmail.com>
2025-09-22 13:30:40 -0300

    corpo do serve montado

---
+++ src/models/dados.js
@@ -0,0 +1,114 @@
+const mangas = [
+    {
+      id: 1,
+      titulo: "One Piece",
+      autor: "Eiichiro Oda",
+      editora: "Shueisha",
+      volumes: 109,
+      genero: "Aventura, Fantasia",
+      status: "Em andamento",
+      anoInicio: 1997,
+      preco: 34.90
+    },
+    {
+      id: 2,
+      titulo: "Berserk",
+      autor: "Kentaro Miura",
+      editora: "Hakusensha",
+      volumes: 42,
+      genero: "Fantasia Sombria",
+      status: "Em andamento",
+      anoInicio: 1989,
+      preco: 69.90
+    },
+    {
+      id: 3,
+      titulo: "Naruto",
+      autor: "Masashi Kishimoto",
+      editora: "Shueisha",
+      volumes: 72,
+      genero: "Ação, Aventura",
+      status: "Concluído",
+      anoInicio: 1999,
+      preco: 29.90
+    },
+    {
+      id: 4,
+      titulo: "Attack on Titan",
+      autor: "Hajime Isayama",
+      editora: "Kodansha",
+      volumes: 34,
+      genero: "Ação, Fantasia Sombria",
+      status: "Concluído",
+      anoInicio: 2009,
+      preco: 39.90
+    },
+    {
+      id: 5,
+      titulo: "Death Note",
+      autor: "Tsugumi Ohba",
+      editora: "Shueisha",
+      volumes: 12,
+      genero: "Suspense, Mistério",
+      status: "Concluído",
+      anoInicio: 2003,
+      preco: 32.50
+    },
+    {
+      id: 6,
+      titulo: "Fullmetal Alchemist",
+      autor: "Hiromu Arakawa",
+      editora: "Square Enix",
+      volumes: 27,
+      genero: "Aventura, Fantasia",
+      status: "Concluído",
+      anoInicio: 2001,
+      preco: 35.00
+    },
+    {
+      id: 7,
+      titulo: "Demon Slayer",
+      autor: "Koyoharu Gotouge",
+      editora: "Shueisha",
+      volumes: 23,
+      genero: "Ação, Fantasia",
+      status: "Concluído",
+      anoInicio: 2016,
+      preco: 30.00
+    },
+    {
+      id: 8,
+      titulo: "Jujutsu Kaisen",
+      autor: "Gege Akutami",
+      editora: "Shueisha",
+      volumes: 27,
+      genero: "Ação, Fantasia Sombria",
+      status: "Em andamento",
+      anoInicio: 2018,
+      preco: 34.90
+    },
+    {
+      id: 9,
+      titulo: "My Hero Academia",
+      autor: "Kohei Horikoshi",
+      editora: "Shueisha",
+      volumes: 40,
+      genero: "Ação, Super-heróis",
+      status: "Em andamento",
+      anoInicio: 2014,
+      preco: 29.90
+    },
+    {
+      id: 10,
+      titulo: "Chainsaw Man",
+      autor: "Tatsuki Fujimoto",
+      editora: "Shueisha",
+      volumes: 17,
+      genero: "Ação, Terror",
+      status: "Em andamento",
+      anoInicio: 2018,
+      preco: 38.00
+    },
+  ];
+  
+  export default { mangas };
